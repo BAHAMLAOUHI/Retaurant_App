85636573732bd322eb7c824f88167518215a3b54
Baha Mlaouhi <bahaeddine.mlaouhi@ensi-uma.tn>
2023-07-11 19:10:03 +0200

    waiter first page

--- src/app/waiter/waiter.page.ts
+++ src/app/waiter/waiter.page.ts
@@ -6,8 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./waiter.page.scss'],
 })
 export class WaiterPage implements OnInit {
+  items = [
+    { name: 'Item1', image: 'https://ionicframework.com/docs/img/demos/thumbnail.svg', state: "Accept", room:26 ,},
+    { name: 'Item2', image: 'https://ionicframework.com/docs/img/demos/thumbnail.svg', state: "Accept", room:12},
+    { name: 'Item3', image: 'https://ionicframework.com/docs/img/demos/thumbnail.svg', state: "Accept", room:13},
+    { name: 'Item4', image: 'https://ionicframework.com/docs/img/demos/thumbnail.svg', state: "Accept", room:2}
+  ];
 
-  constructor() { }
+  toggleItemState(item: any) {
+    if (item.state=="Accept"){
+      item.state = "On the Way";
+    } else if(item.state=="On the Way"){
+      item.state="Delivered" ;
+    } 
+    
+  }
+
+  RemoveItem(items:any,item:any){
+    items.pop(item)
+  }
+    
+  constructor() {}
 
   ngOnInit() {
   }
