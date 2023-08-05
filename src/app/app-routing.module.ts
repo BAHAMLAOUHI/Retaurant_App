a45a3bde16d29328da49e1de85148230a8bb010d
Baha Mlaouhi <bahaeddine.mlaouhi@ensi-uma.tn>
2023-08-05 19:35:17 +0200

    home edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'waiter',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
   {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Device } from '@capacitor/device';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  id: any;
+  battery: any;
+  info: any;
+
+  ngOnInit() {
+    // Call the functions to log the information
+    this.logDeviceInfo();
+    this.logBatteryInfo();
+    this.logDeviceId();
+  }
+
+  logDeviceInfo = async () => {
+    const info = await Device.getInfo();
+    console.log(info);
+    this.info = info;
+  };
+
+  logBatteryInfo = async () => {
+    const info = await Device.getBatteryInfo();
+    console.log(info);
+    this.battery = info;
+  };
+
+  logDeviceId = async () => {
+    try {
+      const uuid = await Device.getId();
+      console.log('Device ID: ' + uuid);
+      this.id = uuid;
+      console.log(this.id);
+    } catch (error) {
+      console.error('Error retrieving device ID:', error);
+    }
+  };
 }
